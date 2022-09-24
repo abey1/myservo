328ea5b2ed8d9cfba05b712f4939a3508b669e28
abey <bruckabey@gmail.com>
2022-09-24 12:50:44 +0300

    first commit

---
+++ sketch_sep23a/sketch_sep23a.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+Servo myservo;
+int analogPin = 0;
+
+void setup() {
+  Serial.begin(9600); // open the serial port at 9600 bps:
+   myservo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // delay(2000);
+  
+  int sensorValue = analogRead(analogPin);
+  
+  float voltage= sensorValue * (5.0 / 1023.0);
+  
+  if(voltage >= 4.8){
+    myservo.write(90);
+    Serial.println("on");
+  }else{
+    myservo.write(0);
+    Serial.println("off");
+  }
+}
